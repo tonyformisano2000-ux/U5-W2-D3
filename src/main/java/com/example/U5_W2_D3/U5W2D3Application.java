caaaaa09dfce22bc4dfc05a4281f2649d27cf463
Antonio <tony.formisano.2000@gmail.com>
2026-07-08 18:43:35 +0200

    Initial commit

---
+++ src/main/java/com/example/U5_W2_D3/U5W2D3Application.java
@@ -0,0 +1,13 @@
+package com.example.U5_W2_D3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W2D3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W2D3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/U5_W2_D3/U5W2D3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.U5_W2_D3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W2D3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
